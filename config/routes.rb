a9e4dca5c3914d275fa5a3591a75b4087875a883
Vishak Seshadri <vishak.ramaiah@gmail.com>
2015-10-04 10:43:19 -0500

    chapter 2 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
+require 'api_constraints'
+
 MarketPlaceApi::Application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                   constraints: { subdomain: 'api' }, path: '/'  do
-    # We are going to list our resources here
+    scope module: :v1 do
+      scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
+    end
   end
 end
